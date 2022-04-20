a4242754291f31640052329f4763aebfba2bfca5
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2022-04-20 15:54:57 +0800

    lib-nothing

---
+++ 1-module-target-lib/1-lib-nothing/index.ts
@@ -0,0 +1,9 @@
+// Note: The "// @ts-expect-error" is a magic comment that
+// tells TS compiler the next line should contain an error.
+// If there is no error, TS compiler throws an error for this.
+
+// @ts-expect-error
+console.log();
+
+// @ts-expect-error
+let one = String(1);
