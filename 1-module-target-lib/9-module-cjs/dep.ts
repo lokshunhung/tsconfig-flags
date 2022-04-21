8204832c94e9c24acaa87f3af0c0c664bc59ea35
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2022-04-21 15:21:07 +0800

    module cjs

---
+++ 1-module-target-lib/9-module-cjs/dep.ts
@@ -0,0 +1,5 @@
+export const foo = "bar";
+
+export function getBiggerFoo() {
+    return foo.toUpperCase();
+}
---
+++ 1-module-target-lib/9-module-cjs/index.ts
@@ -0,0 +1,5 @@
+import * as dep from "./dep";
+
+const text = `Bigger ${dep.foo} is ${dep.getBiggerFoo()}`;
+
+console.log(text);
