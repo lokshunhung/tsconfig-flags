8d3a99f624abaa739b637cf9dd57f4f614922ff9
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2022-05-19 10:58:09 +0800

    update cjs wrapper

--- 1-module-target-lib/9-module-cjs/hand-written-cjs/__node-cjs-globals.js
+++ 1-module-target-lib/9-module-cjs/hand-written-cjs/__node-cjs-globals.js
@@ -1,5 +1,35 @@
-(function (exports, require, module, __filename, __dirname) {
+const moduleRegistry = {};
+
+//
+// The wrapper looks something like this
+//
+
+moduleRegistry["<module_id>"] = function (exports, require, module, __filename, __dirname) {
     //
     // Module code actually lives in here
     //
-});
+};
+
+//
+// The require function looks something like this
+//
+function require(moduleId) {
+    if (moduleId in require.cache) {
+        return require.cache[moduleId];
+    }
+
+    const module = {
+        id: moduleId,
+        filename: "./the/file/to/be/required", // and other stuff...
+        exports: {},
+    };
+    const exports = module.exports;
+    const __filename = module.filename;
+    const __dirname = fs.dirname(__filename);
+
+    const factory = moduleRegistry[moduleId];
+
+    require.cache[moduleId] = factory(exports, require, module, __filename, __dirname);
+    return require.cache[moduleId];
+}
+require.cache = {};
